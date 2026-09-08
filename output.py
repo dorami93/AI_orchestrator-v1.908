6267948d796139665a0f0bd1c79b8e5a349d90f7
dorami93 <is0570vp@ed.ritsumei.ac.jp>
2026-09-08 16:07:58 +0900

    Update output.py

--- output.py
+++ output.py
@@ -1,13 +1,4 @@
-import re
-from js import document
-
-
-def markdown(text):
-    text = text.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;")
-    text = re.sub(r"```(\w*)\n([\s\S]*?)```", r"<pre><code>\2</code></pre>", text)
-    text = re.sub(r"`([^`]+)`", r"<code>\1</code>", text)
-    text = re.sub(r"\*\*(.+?)\*\*", r"<b>\1</b>", text)
-    return text
+from js import document, marked
 
 
 def render_messages(element, messages):
@@ -16,7 +7,7 @@ def render_messages(element, messages):
     for message in messages:
         div = document.createElement("div")
         div.className = "msg " + message["role"]
-        div.innerHTML = markdown(message["content"])
+        div.innerHTML = marked.parse(message["content"])
         element.appendChild(div)
 
     element.scrollTop = element.scrollHeight
